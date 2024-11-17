ec9fbc0b5742abae294ecddc80b080aaa672fcf2
the-abra <aegean.sword1@gmail.com>
2024-11-17 19:08:21 +0300

    ST.5

--- .setup/pht-setup.sh
+++ .setup/pht-setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt update
-apt install net-tools curl -yq
+apt install net-tools curl build-essential -yq
 
 # Check if Rust is already installed
 if command -v rustc > /dev/null 2>&1; then
